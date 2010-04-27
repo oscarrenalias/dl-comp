129bbf0ae480b3000b5a03dbbb2bbd357b505e69
Oscar Renalias <oscar@renalias.net>
2010-04-28 02:09:38 +0300

    Added the possibility to remove items from the shopping cart

--- scala/dl-comp-frontend/src/main/scala/com/accenture/webshop/frontend/snippet/ShoppingCart.scala
+++ scala/dl-comp-frontend/src/main/scala/com/accenture/webshop/frontend/snippet/ShoppingCart.scala
@@ -5,15 +5,29 @@ import scala.collection.mutable.ArrayBuffer
 import _root_.net.liftweb.util.{Box,Full,Empty,Helpers,Log}
 import _root_.scala.xml.{NodeSeq,Text,Node,Elem}
 import _root_.net.liftweb.util.Helpers._
-import scala.actors._
-import scala.collection.mutable.HashMap
+import net.liftweb.http.js.JsCmds._
+import net.liftweb.http.js.jquery.JqJsCmds._
 import com.accenture.webshop.frontend.rest.RestfulItem
 import com.accenture.webshop.frontend.model._
 
-object ShoppingCart extends SessionVar[ShoppingCart](new ShoppingCart) {
+object ShoppingCart extends SessionVar {
+  
+  var items = new ArrayBuffer[RestfulItem]  
+  
   def addItem(item: RestfulItem) = {
-	  get.add(item)	  
+    items += item
+    dumpCartData
   }
+  
+  def removeItem(item: RestfulItem) = {
+    items -= item
+    dumpCartData
+  }
+  
+  def dumpCartData = {
+    Log.debug("Items in cart " + items.size)
+    items.foreach(i => Log.debug("item: " + i.id))
+  }  
 }
 
 /**
@@ -21,30 +35,22 @@ object ShoppingCart extends SessionVar[ShoppingCart](new ShoppingCart) {
  */
 class ShoppingCartData {
   def list(xhtml:NodeSeq): NodeSeq = {
+
+    
     	ShoppingCart.items.flatMap( item =>
     	  bind( "item", xhtml, 
     			"id" -> item.id, 
     			"amount" -> 1.toString,
-    			"description" -> item.desc,
-    			"price" -> item.price.toString )
+    			"description" -> item.desc,       
+    			"price" -> item.price.toString,
+				"remove" -> SHtml.a({() =>
+					ShoppingCart.removeItem(item)	
+					SetHtml("shopping-cart", <lift:embed what="/templates-hidden/cart-data.html" />)}, Text("Remove"))
+			)
     	)
     }
   
   def toHtml: String = {
     {for(i <- ShoppingCart.items) yield <li>{i.id}: {i.desc}</li>}.mkString
   }  
-}
-
-class ShoppingCart {
-  var items = new ArrayBuffer[RestfulItem]
-  
-  def add(item: RestfulItem) = {    
-    items += item
-    dumpCartData
-  }
-  
-  def dumpCartData = {
-    Log.debug("Items in cart " + items.size)
-    items.foreach(i => Log.debug("item: " + i.id))
-  } 
 }
