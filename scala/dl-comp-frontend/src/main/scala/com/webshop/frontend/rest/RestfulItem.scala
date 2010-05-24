7c88a31a204bf7684e3a387c3f43ce6dad6bd9e8
Oscar Renalias <oscar.renalias@accenture.com>
2010-05-24 10:21:37 +0300

    Removed a file and folder that are not needed anymore

--- scala/dl-comp-frontend/src/main/scala/com/webshop/frontend/rest/RestfulItem.scala
+++
@@ -1,6 +0,0 @@
-package com.webshop.frontend.rest
-
-import _root_.net.liftweb.util.{Box,Full,Empty,Log} 
-import scala.collection.mutable.HashMap
-import com.webshop.frontend.restclient.RestClient
-
