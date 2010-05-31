298f9ca0a34d88c6551eafdec291a9ae12c06e4a
Oscar Renalias <oscar@renalias.net>
2010-06-01 00:45:14 +0300

    Added some test cases for the json serializer

--- scala/dl-comp-frontend/src/test/scala/frontend/AppTest.scala
+++ scala/dl-comp-frontend/src/test/scala/frontend/AppTest.scala
@@ -6,15 +6,55 @@ import Assert._
 import _root_.scala.xml.XML
 import _root_.net.liftweb.util._
 import _root_.net.liftweb.common._
+import net.liftweb.json._
+import net.liftweb.json.Serialization.{read, write}
+import net.liftweb.json.JsonAST._
+import net.liftweb.json.JsonAST
+import net.liftweb.json.JsonDSL._  
+import com.webshop.frontend.restclient._
+import com.webshop.frontend.model._
 
 object AppTest {
   def suite: Test = {
     val suite = new TestSuite(classOf[AppTest])
+    suite.addTestSuite(classOf[OrderSerializerTest])
     suite
   }
 
   def main(args : Array[String]) {
     _root_.junit.textui.TestRunner.run(suite)
+  }
+}
+
+class OrderSerializerTest extends TestCase("serializer") {
+  
+  implicit val formats = Serialization.formats(NoTypeHints)      
+  val serializer = new OrderSerializer
+  val f = serializer.serialize  
+  
+  /**
+   * Test serialization of an empty order
+   */
+  def testEmptyOrder() = {
+    val expectedJson = """{"address1":"","address2":"","city":"","postcode":"","phone":"","user":"nouser","country":"","number":"","status":"New","description":"","items":[]}"""
+    val o = new Order
+    val jsonOrder = compact(JsonAST.render(f(o)))    
+    assertEquals(expectedJson, jsonOrder)
+  }
+  
+  /**
+   * Test the serialization of Order objects with line items
+   */
+  def testOrderWithLineItems() = {
+    // build an order object
+    val i1 = new Item("1","Name 1","Description 1","10", "EUR", List())
+    val i2 = new Item("2","Name 2","Description 2","10", "USD", List())
+    val o  = new Order
+    o.items append ((1,i1), (2,i2))
+    
+    val expectedJson = """{"address1":"","address2":"","city":"","postcode":"","phone":"","user":"nouser","country":"","number":"","status":"New","description":"","items":[{"amount":1,"item":{"id":"1"}},{"amount":2,"item":{"id":"2"}}]}"""
+    val jsonOrder = compact(JsonAST.render(f(o)))
+    assertEquals(expectedJson, jsonOrder)
   }
 }
 
