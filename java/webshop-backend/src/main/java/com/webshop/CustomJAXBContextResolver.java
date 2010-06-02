f2d695a9f7d5a8df767676d23817d4680722743e
Oscar Renalias <oscar@renalias.net>
2010-06-02 22:13:27 +0300

    A custom JAXB context resolver that makes parsing on the client side much easier

--- java/webshop-backend/src/main/java/com/webshop/CustomJAXBContextResolver.java
+++ java/webshop-backend/src/main/java/com/webshop/CustomJAXBContextResolver.java
@@ -6,27 +6,34 @@ import javax.xml.bind.JAXBContext;
 
 import com.sun.jersey.api.json.JSONConfiguration;
 import com.sun.jersey.api.json.JSONJAXBContext;
-import com.webshop.catalog.CatalogBean;
 import com.webshop.item.ItemBean;
-import com.webshop.order.AddressInfo;
-import com.webshop.order.ContactInfo;
 import com.webshop.order.OrderBean;
 import com.webshop.order.OrderItemBean;
 
+/**
+ * Custom JAXB context resolver that enables the "natural" json generator. The
+ * natural generator has nicer features, such as generating json arrays always
+ * properly, instead of only when the array has more than one element (makes
+ * the parsing easier)
+ */
 @Provider
 public class CustomJAXBContextResolver implements ContextResolver<JAXBContext> {
 
     private JAXBContext context;
-    private Class[] types = {CatalogBean.class, OrderItemBean.class, ItemBean.class, AddressInfo.class, ContactInfo.class, OrderBean.class};
+    private Class[] types = {OrderBean.class, OrderItemBean.class, ItemBean.class};
 
     public CustomJAXBContextResolver() throws Exception {
-        this.context = new JSONJAXBContext(
-                JSONConfiguration.natural().build(),
-                types);
+        this.context = new JSONJAXBContext(JSONConfiguration.natural().build(), types);
     }
 
     public JAXBContext getContext(Class<?> objectType) {
-        return (types[0].equals(objectType)) ? context : null;
+    	for(Class c : types) {
+    		if(c.equals(objectType)) {
+    			System.out.println("Class found:" + objectType);
+    			return(context);
+    		}
+    	}
+    	return(null);
     }
 }
 
